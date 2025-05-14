d7fefe4597f5713ac0e8139fde927da895927bc7
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2025-05-14 11:13:03 +0200

    Spotify songs i array och objekt

---
+++ SpotifyTest/test.js
@@ -0,0 +1,15 @@
+const trackIds = [
+  "3C3F1scQRdhKRH1IBbszEE",
+  "7ju97lgwC2rKQ6wwsf9no9",
+  "6dGnYIeXmHdcikdzNNDMm2",
+  "1zB4vmk8tFRmM9UULNzbLB",
+  "2uFaJJtFpPDc5Pa95XzTvg",
+];
+
+const ExpectedTrackNames = {
+  "3C3F1scQRdhKRH1IBbszEE": "Cloudy",
+  "7ju97lgwC2rKQ6wwsf9no9": "Rain on me",
+  "6dGnYIeXmHdcikdzNNDMm2": "Here comes the sun",
+  "1zB4vmk8tFRmM9UULNzbLB": "Thunder",
+  "2uFaJJtFpPDc5Pa95XzTvg": "Let it snow",
+};
