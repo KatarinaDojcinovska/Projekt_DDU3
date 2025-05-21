b2363bf1fb87817bc6acf998f21f0c168fd01c02
Katarinawallenhoff <katarina.wallenhoff@outlook.com>
2025-05-21 13:59:58 +0200

    Changed setup
    Changed setup for the test files. Instead of one file where every test is run it's going to be one page for register/login and one for when you're logged in.

--- Test/test-denoAPI.js
+++ Test/test-denoAPI.js
@@ -1,3 +1,5 @@
+
+//LOG IN OCH REGISTER
 const button2 = document.getElementById("logInButton");
 const gifBox = document.getElementById("gif");
 const tryGifBtn = document.querySelector(".try-Gif button");
@@ -7,7 +9,6 @@ const messageBox = document.getElementById("messageBox");
 const messageText = document.getElementById("messageText");
 const closeMessageBtn = document.getElementById("closeMessageBtn")
 
-let currentGifUrl = "";
 
 button2.addEventListener("click", async function () {
   const usernameInput = document.getElementById("username-logIn");
@@ -72,49 +73,3 @@ deleteButton.addEventListener("click", async function () {
   }
 });
 
-saveButton.addEventListener("click", async function () {
-  messageBox.style.display = "block";
-
-  const response = await fetch("http:localhost:8000/save-gif", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  });
-
-  const result = await response.json();
-
-  if (response.status === 200) {
-    messageText.textContent = "GIF saved!";
-  } else if (response.status === 404) {
-    messageText.textContent = "User not found";
-  } else {
-    messageText.textContent = result.error;
-  }
-});
-
-async function bringGIF() {
-  const apiKey = "AIzaSyB0rByOPuVe1syMsx5CntyK69GUbPecxN8";
-  const searchTerm = "sunny weather";
-  const url = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${apiKey}&limit=1&random=true`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    currentGifUrl = data.results[0].media_formats.tinygif.url;
-    gifBox.innerHTML = "";
-
-    const img = document.createElement("img");
-    img.src = currentGifUrl;
-    img.style.width = "200px";
-    gifBox.appendChild(img);
-
-    console.log("GIF hämtad:", currentGifUrl);
-  } catch (err) {
-    console.error("Fel vid GIF-hämtning:", err);
-  }
-}
-
-tryGifBtn.addEventListener("click", bringGIF);
-closeMessageBtn.addEventListener("click", function () {
-  messageBox.style.display = "none";
-});
