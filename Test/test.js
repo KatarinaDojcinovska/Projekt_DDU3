726c7d9df1b9e266ae6b871ea0362df62e4e24d3
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2025-05-26 17:24:41 +0200

    Test fungerar
    Test fungerar men, om username och password ska försvinna för register vid omstart av servern så fungerar inte det. Det get Conflict för att username och password redan finns

--- Test/test.js
+++ Test/test.js
@@ -1,181 +1,182 @@
-//Klar till 99% lite språk ändring osv
-const registerButton = document.querySelector("#registerButton");
-const logInButton = document.getElementById("logInButton");
-const tryGifBtn = document.querySelector("#tryGifbtn");
-const saveGifButton = document.getElementById("saveGifbtn");
-const deleteGifButton = document.getElementById("deleteGifbtn");
-
 const gifBox = document.getElementById("gif");
-const messageBox = document.getElementById("messageBox");
-const messageText = document.getElementById("messageText");
-const closeMessageBtn = document.getElementById("closeMessageBtn");
-const usernameInput = document.querySelector("#username-Registration");
-const passwordInput = document.querySelector("#password-Registration");
 
-//register
 async function register() {
-  const username = usernameInput.value;
-  const password = passwordInput.value;
-
-  const response = await fetch("http://localhost:8000/register", {
+  const response = await fetch("http:localhost:8000/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name: username, password: password}),
+    body: JSON.stringify({ username: "sun", password: "moon" }),
   });
-  const result = await response.json();
 
-  messageBox.style.display = "block";
+  const data = await response.json();
 
   if (response.status === 200) {
-    messageText.textContent = "Register succeeded!";
-    usernameInput.value = "";
-    passwordInput.value = "";
-  } else if (response.status === 409) {
-    messageText.textContent = "User already exists!";
-    usernameInput.value = "";
-    passwordInput.value = "";
+    console.log("sucess", data);
   } else {
-    messageText.textContent = result.error || "Registration failed";
+    console.log("user already exists, conflict", data);
   }
+  login();
+}
+register();
+
+async function login() {
+  const response = await fetch("http://localhost:8000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username: "sun", password: "moon" }),
+  });
+
+  const data = await response.json();
+
+  if (response.status === 200) {
+    console.log("success", data);
+  } else if (response.status === 400) {
+    console.log("username and password required", data);
+  } else {
+    console.log("Invalid password", data);
+  }
+ getGIF()
 }
 
-registerButton.addEventListener("click", register);
+async function getGIF() {
+  let currentGifUrl;
 
-closeMessageBtn.addEventListener("click", function () {
-  messageBox.style.display = "none";
-});
-
-//login
-logInButton.addEventListener("click", async function () {
-  const usernameInput = document.getElementById("username-logIn");
-  const passwordInput = document.getElementById("password-logIn");
-
-  const username = usernameInput.value.trim();
-  const password = passwordInput.value.trim();
-
-  if (!username || !password) {
-    alert("Vänligen fyll i både användarnamn och lösenord.");
-    return;
-  }
-
-  usernameInput.value = "";
-  passwordInput.value = "";
-
-  let logInUser = JSON.stringify({
-      username: username, 
-      password: password
-    });
-
-
-  const url = `http://localhost:8000/login`;
-
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: logInUser,
-    });
-
-    const result = await response.json();
-  messageBox.style.display = "block";
-
-
-    if (response.status === 200) {
-      messageText.textContent = "Login succeeded!";
-      console.log("Inloggning lyckades:", result);
-    } else {
-      messageText.textContent = "Login failed!";
-      console.error("Inloggning misslyckades:", result.error);
-     //alert("Inloggning misslyckades - användaren finns inte");
-    }
-  } catch (err) {
-    console.error("Något gick fel:", err);
-  }
-});
-
-//try hämta gif
-let currentGifUrl = null;
-
-async function bringGIF() {
   const apiKey = "AIzaSyB0rByOPuVe1syMsx5CntyK69GUbPecxN8";
   const searchTerm = "sunny weather";
   const url = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${apiKey}&limit=1&random=true`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  const data = await response.json();
 
-    currentGifUrl = data.results[0].media_formats.tinygif.url;
-    gifBox.innerHTML = "";
+  currentGifUrl = data.results[0].media_formats.tinygif.url;
+  gifBox.innerHTML = "";
 
-    const img = document.createElement("img");
-    img.src = currentGifUrl;
-    img.style.width = "200px";
-    gifBox.appendChild(img);
+  const img = document.createElement("img");
+  img.src = currentGifUrl;
+  img.style.width = "200px";
+  gifBox.appendChild(img);
 
-    console.log("GIF hämtad:", currentGifUrl);
-  } catch (err) {
-    console.error("Fel vid GIF-hämtning:", err);
+  console.log("GIF hämtad:", currentGifUrl);
+  saveGIF();
+}
+
+
+async function saveGIF() {
+  const response = await fetch("http://localhost:8000/save-gif", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username: "sun", gifUrl:"https://media.tenor.com/hdf7HJw5TL0AAAAM/weather-nw.gif"}),
+  });
+
+  const data = await response.json();
+
+  if (response.status === 200) {
+    console.log("sucess, the GIF is saved", data);
+  } else {
+    console.log("user not found", data);
+  }
+  deleteGIF()
+}
+
+async function deleteGIF(){
+  const response = await fetch("http://localhost:8000/delete-gif", {
+    method: "DELETE",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({username: "sun", gifUrl: "https://media.tenor.com/hdf7HJw5TL0AAAAM/weather-nw.gif"})
+  })
+
+  const data = await response.json()
+
+  if(response.status === 200){
+    console.log("delete succeeded", data)
+  } else {
+    console.log("user not found", data)
   }
 }
 
-tryGifBtn.addEventListener("click", bringGIF);
+// //try hämta gif
+// let currentGifUrl = null;
 
-//saveGIF
-saveGifButton.addEventListener("click", async function () {
-  if (!currentGifUrl) {
-    alert("Ingen GIF att spara.");
-    return;
-  }
+// async function bringGIF() {
+//   const apiKey = "AIzaSyB0rByOPuVe1syMsx5CntyK69GUbPecxN8";
+//   const searchTerm = "sunny weather";
+//   const url = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${apiKey}&limit=1&random=true`;
 
-  messageBox.style.display = "block";
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
 
-  const gifUrl = JSON.stringify({ gifUrl: currentGifUrl });
+//     currentGifUrl = data.results[0].media_formats.tinygif.url;
+//     gifBox.innerHTML = "";
 
-  try {
-    const response = await fetch("http://localhost:8000/save-gif", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: gifUrl,
-    });
+//     const img = document.createElement("img");
+//     img.src = currentGifUrl;
+//     img.style.width = "200px";
+//     gifBox.appendChild(img);
 
-    const result = await response.json();
+//     console.log("GIF hämtad:", currentGifUrl);
+//   } catch (err) {
+//     console.error("Fel vid GIF-hämtning:", err);
+//   }
+// }
 
-    if (response.status === 200) {
-      messageText.textContent = "GIF saved!";
-    } else if (response.status === 404) {
-      messageText.textContent = "User not found";
-    } else {
-      messageText.textContent = result.error;
-    }
-  } catch (err) {
-    messageText.textContent = "Ett fel uppstod vid sparande.";
-    console.error(err);
-  }
-});
+// tryGifBtn.addEventListener("click", bringGIF);
 
-closeMessageBtn.addEventListener("click", function () {
-  messageBox.style.display = "none";
-});
+// //saveGIF
+// saveGifButton.addEventListener("click", async function () {
+//   if (!currentGifUrl) {
+//     alert("Ingen GIF att spara.");
+//     return;
+//   }
 
-//delete
-deleteGifButton.addEventListener("click", async function () {
-  const response = await fetch("http:localhost:8000/delete-gif", {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-  });
+//   messageBox.style.display = "block";
 
-  messageBox.style.display = "block";
+//   const gifUrl = JSON.stringify({ gifUrl: currentGifUrl });
 
-  const result = await response.json();
+//   try {
+//     const response = await fetch("http://localhost:8000/save-gif", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: gifUrl,
+//     });
 
-  if (response.status === 200) {
-    messageText.textContent = "GIF Deleted";
-  } else if (response.status === 404) {
-    messageText.textContent = "User not found";
-  } else {
-    messageText.textContent = result.error;
-  }
-});
+//     const result = await response.json();
+
+//     if (response.status === 200) {
+//       messageText.textContent = "GIF saved!";
+//     } else if (response.status === 404) {
+//       messageText.textContent = "User not found";
+//     } else {
+//       messageText.textContent = result.error;
+//     }
+//   } catch (err) {
+//     messageText.textContent = "Ett fel uppstod vid sparande.";
+//     console.error(err);
+//   }
+// });
+
+// closeMessageBtn.addEventListener("click", function () {
+//   messageBox.style.display = "none";
+// });
+
+// //delete
+// deleteGifButton.addEventListener("click", async function () {
+//   const response = await fetch("http:localhost:8000/delete-gif", {
+//     method: "DELETE",
+//     headers: { "Content-Type": "application/json" },
+//   });
+
+//   messageBox.style.display = "block";
+
+//   const result = await response.json();
+
+//   if (response.status === 200) {
+//     messageText.textContent = "GIF Deleted";
+//   } else if (response.status === 404) {
+//     messageText.textContent = "User not found";
+//   } else {
+//     messageText.textContent = result.error;
+//   }
+// });
