552aacecaf50973e62bb74465f4f85d5959af7a6
Jud02 <judy.ynh@gmail.com>
2025-05-26 21:32:33 +0200

    fixar plats

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,23 @@
-// Så kan användningen av klassen weatherClass se ut (troligen det vi kommer att använda)
-
-import { WeatherClass } from "./weatherClass.js";
+import { WeatherClass } from "./Classes/weatherClass.js";
 
 window.addEventListener("load", function() {
-  // 1) Skapa en instans med din API-nyckel
-  var weather = new WeatherClass("59a2034ae4aa4ce49c6215358251305");
+  let weather = new WeatherClass("59a2034ae4aa4ce49c6215358251305");
 
+  const compassDiv = document.getElementById("compass");
+
+  // 3) Kör hämtningen
   weather.getWeather(function(err) {
     if (err) {
       console.log("Fel vid hämtning:", weather.error);
     } else {
-      // All data finns nu i instansen
-      console.log("Plats:    " + weather.location);
-      console.log("Temp (C): " + weather.temp);
-      console.log("Väder:    " + weather.condition);
-      // Vi kan nu spara dessa i variabler, localStorage eller skicka vidare och appenda eller liknande
+      const locationName    = weather.location;
+      const temperatureC    = weather.temp;
+      const weatherCondition = weather.condition;
+
+      // 4) Skapa ett <p>-element och visa platsen i #compass
+      const p = document.createElement("p");
+      p.textContent = weather.location;
+      compassDiv.appendChild(p);
     }
   });
 });
