12dfd6d2fe198281ada158536a5c2bd971d993ac
Katarinawallenhoff <katarina.wallenhoff@outlook.com>
2025-05-27 10:42:42 +0200

    Changes of test

--- Test/test.js
+++ Test/test.js
@@ -1,3 +1,9 @@
+
+function writeMessage(data){
+  let mainElement = document.querySelector("main");
+}
+
+
 const gifBox = document.getElementById("gif");
 
 async function register() {
@@ -96,87 +102,4 @@ async function deleteGIF(){
   }
 }
 
-// //try hämta gif
-// let currentGifUrl = null;
 
-// async function bringGIF() {
-//   const apiKey = "AIzaSyB0rByOPuVe1syMsx5CntyK69GUbPecxN8";
-//   const searchTerm = "sunny weather";
-//   const url = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${apiKey}&limit=1&random=true`;
-
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-
-//     currentGifUrl = data.results[0].media_formats.tinygif.url;
-//     gifBox.innerHTML = "";
-
-//     const img = document.createElement("img");
-//     img.src = currentGifUrl;
-//     img.style.width = "200px";
-//     gifBox.appendChild(img);
-
-//     console.log("GIF hämtad:", currentGifUrl);
-//   } catch (err) {
-//     console.error("Fel vid GIF-hämtning:", err);
-//   }
-// }
-
-// tryGifBtn.addEventListener("click", bringGIF);
-
-// //saveGIF
-// saveGifButton.addEventListener("click", async function () {
-//   if (!currentGifUrl) {
-//     alert("Ingen GIF att spara.");
-//     return;
-//   }
-
-//   messageBox.style.display = "block";
-
-//   const gifUrl = JSON.stringify({ gifUrl: currentGifUrl });
-
-//   try {
-//     const response = await fetch("http://localhost:8000/save-gif", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: gifUrl,
-//     });
-
-//     const result = await response.json();
-
-//     if (response.status === 200) {
-//       messageText.textContent = "GIF saved!";
-//     } else if (response.status === 404) {
-//       messageText.textContent = "User not found";
-//     } else {
-//       messageText.textContent = result.error;
-//     }
-//   } catch (err) {
-//     messageText.textContent = "Ett fel uppstod vid sparande.";
-//     console.error(err);
-//   }
-// });
-
-// closeMessageBtn.addEventListener("click", function () {
-//   messageBox.style.display = "none";
-// });
-
-// //delete
-// deleteGifButton.addEventListener("click", async function () {
-//   const response = await fetch("http:localhost:8000/delete-gif", {
-//     method: "DELETE",
-//     headers: { "Content-Type": "application/json" },
-//   });
-
-//   messageBox.style.display = "block";
-
-//   const result = await response.json();
-
-//   if (response.status === 200) {
-//     messageText.textContent = "GIF Deleted";
-//   } else if (response.status === 404) {
-//     messageText.textContent = "User not found";
-//   } else {
-//     messageText.textContent = result.error;
-//   }
-// });
